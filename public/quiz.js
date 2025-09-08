4b1dc068784f5033e68ec0eea8e36e49f3eb6f9e
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-09-08 09:38:57 -0300

    Add files via upload

---
+++ public/quiz.js
@@ -0,0 +1,28 @@
+async function maisPergunta(){
+    const fml = document.getElementById("formolario");
+    const totalp = fml.querySelectorAll('.pergunta').length;
+
+    if(totalp > 1){
+        const divpassado = document.getElementById("divpe");
+        divpassado /*continuar isso aq*/
+    }
+
+    fml.innerHTML += `
+        <div id="divpe">
+            <p>Pergunta ${totalp}</p>
+            <input type="text" class="en" placeholder="enunciado">
+            <button onclick="maisAlternativa()">Adicionar Alternativa</button>
+        </div>
+    `
+}
+
+async function maisAlternativa(){
+    const div = document.getElementById("div");
+    const totalp = fml.querySelectorAll('.pergunta').length;
+
+    fml.innerHTML += `
+        <p>Pergunta ${totalp}</p>
+        <input type="text" class="en" placeholder="enunciado">
+        <button onclick="maisAlternativa()">Adicionar Alternativa</button>
+    `
+}
