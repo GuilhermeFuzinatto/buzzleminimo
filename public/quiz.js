d9e0c4fd42bf7bf6b202e8254497309b2127c4d6
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-09-15 07:43:35 -0300

    Add files via upload

--- public/quiz.js
+++ public/quiz.js
@@ -2,14 +2,14 @@ async function maisPergunta(){
     const fml = document.getElementById("formolario");
     const totalp = fml.querySelectorAll('.pergunta').length;
 
-    if(totalp > 1){
+    if(totalp >= 1){
         const divpassado = document.getElementById("divpe");
         divpassado.removeAttribute("id");
     }
 
     fml.innerHTML += `
         <div id="divpe">
-            <p>Pergunta ${totalp}</p>
+            <p class="pergunta">Pergunta ${totalp}</p>
             <input type="text" class="en" placeholder="enunciado">
             <button onclick="maisAlternativa()">Adicionar Alternativa</button>
         </div>
