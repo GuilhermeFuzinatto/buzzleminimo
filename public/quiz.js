1f4bd1993f30ae3d1a31e55f7ab6fd9a30f04100
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-09-15 09:40:07 -0300

    Add files via upload

--- public/quiz.js
+++ public/quiz.js
@@ -5,23 +5,26 @@ async function maisPergunta(){
     if(totalp > 1){
         const divpassado = document.getElementById("divpe");
         divpassado.removeAttribute("id");
+        const divapassado = document.getElementById("diva");
+        divapassado.removeAttribute("id");
     }
 
-    fml.insertAdjacentHTML(beforeend, `
+    fml.insertAdjacentHTML("beforeend", `
         <div id="divpe">
             <p class="pergunta">Pergunta ${totalp + 1}</p>
             <input type="text" class="en" placeholder="enunciado">
+            <div class="diva"></div>
             <button type="button" onclick="maisAlternativa()">Adicionar Alternativa</button>
         </div>
     `);
 }
 
 async function maisAlternativa(){
-    const div = document.getElementById("div");
-    const totalp = fml.querySelectorAll('.pergunta').length;
+    const divatual = document.getElementById(".diva");
+    const totala = divatual.querySelectorAll('.alt').length;
 
     fml.innerHTML += `
-        <p>Pergunta ${totalp}</p>
+        <p class="alt">Alternativa ${totala + 1}</p>
         <input type="text" class="en" placeholder="enunciado">
         <button onclick="maisAlternativa()">Adicionar Alternativa</button>
     `
